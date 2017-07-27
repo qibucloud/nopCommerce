a66e27ad25b8d17995563f56316c815e3c108d54
AndreiMaz <andrey.maz@gmail.com>
2017-07-27 10:46:54 +0300

    Nested "Address" property validation

--- src/Presentation/Nop.Web/Areas/Admin/Models/Customers/CustomerAddressModel.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Models/Customers/CustomerAddressModel.cs
@@ -1,8 +1,11 @@
-﻿using Nop.Web.Areas.Admin.Models.Common;
+﻿using FluentValidation.Attributes;
+using Nop.Web.Areas.Admin.Models.Common;
+using Nop.Web.Areas.Admin.Validators.Customers;
 using Nop.Web.Framework.Mvc.Models;
 
 namespace Nop.Web.Areas.Admin.Models.Customers
 {
+    [Validator(typeof(CustomerAddressValidator))]
     public partial class CustomerAddressModel : BaseNopModel
     {
         public int CustomerId { get; set; }
